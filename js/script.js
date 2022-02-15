a66a9536d643bb110bec8bc9431100d2d1dc44a1
iamraihan <trueraihan@gmail.com>
2022-02-15 21:18:10 +0600

    memory price aded

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.getElementById('eightGB').addEventListener('click', function () {
+
+    let memoryPrice8gb = document.getElementById('memory-cost')
+    memoryPrice8gb = memoryPrice8gb.innerText = 200
+})
+
+document.getElementById('sixteenGB').addEventListener('click', function () {
+    let memory16gb = document.getElementById('memory-cost')
+    memory16gb = memory16gb.innerText = 300
+})
